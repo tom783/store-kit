ce626b2a18a1127724dade7e9d8d1861b88444f9
HellGround <dlgudwns783@gmail.com>
2022-05-16 17:30:35 +0900

    20220516 - 폴더 구조 수정 및 라우터 추가

---
+++ src/containers/pages/Main.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect } from 'react';
+
+export default function Main() {
+  return <div>Main</div>;
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
@@ -9,6 +8,7 @@ import { createGlobalStyle } from 'styled-components';
 import resetCss from 'sanitize.css';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 const GlobalStyles = createGlobalStyle`
  ${resetCss}
@@ -33,6 +33,17 @@ const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
+    <HelmetProvider>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;500;700&display=swap"
+          rel="stylesheet"
+        />
+        <title>Store Kit</title>
+      </Helmet>
+    </HelmetProvider>
     <Provider store={store}>
       <QueryClientProvider client={queryClient}>
         <ReactQueryDevtools initialIsOpen={false} />
